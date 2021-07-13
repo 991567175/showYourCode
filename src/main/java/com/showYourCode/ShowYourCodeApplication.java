99ce78232c62c78a39a9dfc893e4f44d49580cd4
肖腾飞 <xiaotf@833006>
2021-07-13 16:37:07 +0800

    .

--- src/main/java/com/showYourCode/ShowYourCodeApplication.java
+++ src/main/java/com/showYourCode/ShowYourCodeApplication.java
@@ -2,8 +2,9 @@ package com.showYourCode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ShowYourCodeApplication {
 
     public static void main(String[] args) {
